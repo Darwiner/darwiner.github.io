8f534567cd314b38b53548b99e28322f11f6b756
Mike Nguyen <moozoo@gmail.com>
2024-08-28 11:02:09 -0400

    feat: initial commit

---
+++ test/npm/rebilly.js
@@ -0,0 +1,22 @@
+import RebillyInstruments from '@rebilly/instruments';
+// Mount Rebilly Instruments
+RebillyInstruments.mount({
+  publishableKey: 'pk_sandbox_1C0lQkYH59ir1Jrb7c_mmIE80D3PBU6P8KJbLZR',
+  organizationId: 'phronesis-rentavilla',
+  websiteId: 'darwiner-github-io',
+  apiMode: 'sandbox',
+  items: [
+    {
+      planId: 'premium',
+      quantity: 1
+    },
+  ]
+});
+// Optional
+RebillyInstruments.on('instrument-ready', (instrument) => {
+  console.info('instrument-ready', instrument);
+});
+RebillyInstruments.on('purchase-completed', (purchase) => {
+  console.info('purchase-completed', purchase);
+});
+
